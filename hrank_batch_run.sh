867f90acdb25100b03a1f2fcc3089a03f18dd3f4
CsKLevente <67876667+CsKLevente@users.noreply.github.com>
2023-12-06 13:43:58 +0100

    Current changes

---
+++ hrank_batch_run.sh
@@ -0,0 +1,13 @@
+python main.py --job_dir ./result/resnet_56_cr02/ --arch resnet_56 --compress_rate [0.2]*55 --resume ./pretrained/resnet_56.pt.pt
+
+python main.py --job_dir ./result/resnet_56_cr04/ --arch resnet_56 --compress_rate [0.4]*55 --resume ./pretrained/resnet_56.pt.pt
+
+python main.py --job_dir ./result/resnet_56_cr06/ --arch resnet_56 --compress_rate [0.6]*55 --resume ./pretrained/resnet_56.pt.pt
+
+python main.py --job_dir ./result/resnet_56_cr08/ --arch resnet_56 --compress_rate [0.8]*55 --resume ./pretrained/resnet_56.pt.pt
+
+python main.py --job_dir ./result/resnet_56_cr09/ --arch resnet_56 --compress_rate [0.9]*55 --resume ./pretrained/resnet_56.pt.pt
+
+python main.py --job_dir ./result/resnet_56_cr0936/ --arch resnet_56 --compress_rate [0.936]*55 --resume ./pretrained/resnet_56.pt.pt
+
+python main.py --job_dir ./result/resnet_56_cr0995904/ --arch resnet_56 --compress_rate [0.995904]*55 --resume ./pretrained/resnet_56.pt.pt
